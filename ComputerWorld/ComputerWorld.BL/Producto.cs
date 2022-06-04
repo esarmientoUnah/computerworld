6904bd2b3028da9368ae7c7457d7b4abb4d17364
esarmientoUnah <ej.ramos@unah.hn>
2022-06-03 22:24:08 -0600

    Agregando arquitectura basica

--- ComputerWorld/ComputerWorld.BL/Producto.cs
+++ ComputerWorld/ComputerWorld.BL/Producto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ComputerWorld.Web.Models
+namespace ComputerWorld.BL
 {
-    public class ProductoModel
+    public class Producto
     {
         public int Id { get; set; }
         public string Descripcion { get; set; }
+        public int Precio { get; set; }
+        
     }
 }
--- ComputerWorld/ComputerWorld.Web/Controllers/ProductosController.cs
+++ ComputerWorld/ComputerWorld.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using ComputerWorld.Web.Models;
+﻿using ComputerWorld.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace ComputerWorld.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Computadora a";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Computadora b";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Computadora c";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
