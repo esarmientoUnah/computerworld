9959519eb83d7ba7cf65eb92f55e936d0bb853cb
esarmientoUnah <ej.ramos@unah.hn>
2022-06-04 13:16:29 -0600

    No me logra conectar con BD

--- ComputerWorld/ComputerWorld.BL/ProductosBL.cs
+++ ComputerWorld/ComputerWorld.BL/ProductosBL.cs
@@ -8,30 +8,16 @@ namespace ComputerWorld.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Computadora a";
-            producto1.Precio = 20000;
-
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Computadora b";
-            producto2.Precio = 22000;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Computadora c";
-            producto3.Precio = 25000;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            return _contexto.Productos.ToList();
         }
     }
 }
