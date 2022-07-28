a8ce9136bbfb7ce6cde1eaef83d08d91ec03dd65
eramo <eramo@ERICK>
2022-07-27 21:26:34 -0600

    Creando usuarios en web admin

--- ComputerWorld/ComputerWorld.BL/DatosdeInicio.cs
+++ ComputerWorld/ComputerWorld.BL/DatosdeInicio.cs
@@ -14,8 +14,29 @@ namespace ComputerWorld.BL
             var nuevoUsuario = new Usuario();
             nuevoUsuario.Nombre = "admin";
             nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
-
             contexto.Usuarios.Add(nuevoUsuario);
+
+            var nuevoUsuario1 = new Usuario();
+            nuevoUsuario1.Nombre = "erick";
+            nuevoUsuario1.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario1);
+
+            var nuevoUsuario2 = new Usuario();
+            nuevoUsuario2.Nombre = "wilbert";
+            nuevoUsuario2.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario2);
+
+            var nuevoUsuario3 = new Usuario();
+            nuevoUsuario3.Nombre = "emilsson";
+            nuevoUsuario3.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario3);
+
+            var nuevoUsuario4 = new Usuario();
+            nuevoUsuario4.Nombre = "elvin";
+            nuevoUsuario4.Contrasena = Encriptar.CodificarContrasena("123");
+            contexto.Usuarios.Add(nuevoUsuario4);
+
+
             base.Seed(contexto);
         }
     }
